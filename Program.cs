bf2259b1cd79938d8185c3d52d5086152c002b1c
kokhooi <chewkokhooi@gmail.com>
2020-12-20 13:35:18 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -29,14 +29,21 @@ namespace ArraySum
 
             var partSum = arr.Sum() / 2;
             var partArr = new int[arr.Length / 2];
-            var result = getFirstEvenSum(partArr, arr, partSum, 0, arr.Length - 1, 0).ToList();
+            var result = getFirstEvenSum(partArr, arr, partSum, 0, arr.Length - 1, 0);
+
+            if (result == null)
+            {
+                return "-1";
+            }
+
+            var returnElement = result.ToList();
 
             for (var i = 0; i < arr.Length; i++)
             {
                 var elementExisted = false;
-                for (var j = 0; j < result.Count; j++)
+                for (var j = 0; j < returnElement.Count; j++)
                 {
-                    if (result[j] == arr[i])
+                    if (returnElement[j] == arr[i])
                     {
                         elementExisted = true;
                         break;
@@ -45,11 +52,11 @@ namespace ArraySum
 
                 if (!elementExisted)
                 {
-                    result.Add(arr[i]);
+                    returnElement.Add(arr[i]);
                 }
             }
 
-            return string.Join(",", result);
+            return string.Join(",", returnElement);
         }
 
         static int[] getFirstEvenSum(int[] currentArray, int[] originalArray, int partSum, int start, int end, int currentIndex)
